5a50c2c6fe44b4893edb80bb01504c0054ae328c
Natalia Braz <naabraz@gmail.com>
2023-05-15 09:12:27 -0300

    add method to edit

--- src/paginas/Administracao/Restaurantes/AdministracaoRestaurantes.tsx
+++ src/paginas/Administracao/Restaurantes/AdministracaoRestaurantes.tsx
@@ -11,6 +11,7 @@ import {
 
 import IRestaurante from '../../../interfaces/IRestaurante';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 const AdministracaoRestaurantes = () => {
   const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([]);
@@ -27,12 +28,18 @@ const AdministracaoRestaurantes = () => {
         <TableHead>
           <TableRow>
             <TableCell>Nome</TableCell>
+            <TableCell>Editar</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {restaurantes.map((restaurante) => (
             <TableRow key={restaurante.id}>
               <TableCell>{restaurante.nome}</TableCell>
+              <TableCell>
+                [
+                <Link to={`/admin/restaurantes/${restaurante.id}`}>editar</Link>
+                ]
+              </TableCell>
             </TableRow>
           ))}
         </TableBody>
