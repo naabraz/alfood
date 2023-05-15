ca8846b58a395a7c10ca90c4b9ebb00d91d61b27
Natalia Braz <naabraz@gmail.com>
2023-05-15 09:39:50 -0300

    adjust form

--- src/paginas/Administracao/Restaurantes/FormularioRestaurante.tsx
+++ src/paginas/Administracao/Restaurantes/FormularioRestaurante.tsx
@@ -1,4 +1,4 @@
-import { Button, TextField } from '@mui/material';
+import { Box, Button, TextField, Typography } from '@mui/material';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
@@ -38,17 +38,31 @@ const FormularioRestaurante = () => {
   };
 
   return (
-    <form onSubmit={aoSubmeterForm}>
-      <TextField
-        value={nomeRestaurante}
-        onChange={(evento) => setNomeRestaurante(evento.target.value)}
-        label='Nome do Restaurante'
-        variant='standard'
-      />
-      <Button type='submit' variant='outlined'>
-        Salvar
-      </Button>
-    </form>
+    <Box
+      sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}
+    >
+      <Typography component='h1' variant='h6'>
+        Formulário de Restaurantes
+      </Typography>
+      <Box component='form' onSubmit={aoSubmeterForm}>
+        <TextField
+          value={nomeRestaurante}
+          onChange={(evento) => setNomeRestaurante(evento.target.value)}
+          label='Nome do Restaurante'
+          variant='standard'
+          fullWidth
+          required
+        />
+        <Button
+          sx={{ marginTop: 1 }}
+          type='submit'
+          variant='outlined'
+          fullWidth
+        >
+          Salvar
+        </Button>
+      </Box>
+    </Box>
   );
 };
 
