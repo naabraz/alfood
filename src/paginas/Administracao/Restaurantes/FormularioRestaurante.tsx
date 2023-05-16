e9a0edc15fead73e5ee188a8bcd683910d30c0fd
Natalia Braz <naabraz@gmail.com>
2023-05-16 09:28:32 -0300

    chore: add app bar

--- src/paginas/Administracao/Restaurantes/FormularioRestaurante.tsx
+++ src/paginas/Administracao/Restaurantes/FormularioRestaurante.tsx
@@ -38,12 +38,17 @@ const FormularioRestaurante = () => {
 
   return (
     <Box
-      sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        flexGrow: 1,
+      }}
     >
       <Typography component='h1' variant='h6'>
         Formulário de Restaurantes
       </Typography>
-      <Box component='form' onSubmit={aoSubmeterForm}>
+      <Box component='form' sx={{ width: '100%' }} onSubmit={aoSubmeterForm}>
         <TextField
           value={nomeRestaurante}
           onChange={(evento) => setNomeRestaurante(evento.target.value)}
